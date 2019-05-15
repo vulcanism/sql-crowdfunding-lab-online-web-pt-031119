75178912f62997d574ac9825955a034896b6d6b5
Savannah C <sgcorrell@gmail.com>
2019-05-15 20:30:11 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,7 +10,9 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 "SELECT title
 FROM projects
 INNER JOIN pledges
-ON project_id = pledges.project_id"
+ON project_id = pledges.project_id
+GROUP BY projects.id
+ORDER BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
